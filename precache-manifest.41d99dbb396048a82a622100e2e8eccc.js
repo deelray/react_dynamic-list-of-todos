7ccfd5fbd34a4202d66a92972ae15ea5416f7c52
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-28 01:49:58 +0200

    Updates

---
+++ precache-manifest.41d99dbb396048a82a622100e2e8eccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc943c6ffb4efde7e261428f503d8c1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a9429bcb5c0bb92bbc0",
+    "url": "./static/css/main.214c70b5.chunk.css"
+  },
+  {
+    "revision": "70c67382686ede93ce33",
+    "url": "./static/js/2.121656e4.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.121656e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9429bcb5c0bb92bbc0",
+    "url": "./static/js/main.34a41106.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
